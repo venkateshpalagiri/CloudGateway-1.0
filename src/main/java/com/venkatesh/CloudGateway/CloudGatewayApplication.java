2dac97a1be70b53d37300f52d54f7709a48d76a8
Venkat Palagiri <44697660+venkateshpalagiri@users.noreply.github.com>
2024-04-11 11:44:07 -0400

    ReateLimiter-Resilience4j

--- src/main/java/com/venkatesh/CloudGateway/CloudGatewayApplication.java
+++ src/main/java/com/venkatesh/CloudGateway/CloudGatewayApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class CloudGatewayApplication {
@@ -14,7 +16,10 @@ public class CloudGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudGatewayApplication.class, args);
 	}
-
+	@Bean
+	KeyResolver userKeySolver(){
+		return exchange -> Mono.just("userKey");
+	}
 	@Bean
 	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer(){
 		return factory -> factory.configureDefault(
